74cc11aab281616403b6c6cf17a7fadd9160dd78
jlatala <12474086+jlatala@users.noreply.github.com>
2021-05-16 22:58:32 +0200

    adding tests for credit with fixed installments

---
+++ tests/test_fixed_credit.py
@@ -0,0 +1,33 @@
+import unittest
+from collections.abc import Iterator
+from credit_calculator.src.fixed_credit import FixedCredit
+
+
+class TestFixedCredit(unittest.TestCase):
+
+    def setUp(self):
+        self.c1 = FixedCredit(300_000, 30, 0.04)
+
+    def test_all_values(self):
+        self.assertEqual([i.value for i in self.c1], [
+                         1432.25 for _ in range(360)])
+
+    def test_fixed_credit_is_iterator(self):
+        self.assertIsInstance(self.c1, Iterator)
+
+    def test_number_of_installments(self):
+        for i, _ in enumerate(self.c1):
+            pass
+
+        self.assertEqual(i, 359)
+        with self.assertRaises(StopIteration):
+            next(self.c1)
+
+    def test_single_installment(self):
+        self.assertEqual(next(self.c1).value, 1432.25)
+        # should not change over next payments
+        self.assertEqual(next(self.c1).value, 1432.25)
+
+
+if __name__ == '__main__':
+    unittest.main()
