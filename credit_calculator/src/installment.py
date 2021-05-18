3bd5b80e9e1dda6a3f44af8e3f8921cd275245d0
jlatala <12474086+jlatala@users.noreply.github.com>
2021-05-18 11:43:59 +0200

    handle float rounding

--- credit_calculator/src/installment.py
+++ credit_calculator/src/installment.py
@@ -1,32 +1,32 @@
-
-def round_currency(func):
-    def rounded(*args, **kwargs):
-        return float(round(func(*args, **kwargs), 2))
-
-    return rounded
+from .utils import round_currency
 
 
 class Installment:
     def __init__(self, principal, interest):
-        self.__principal = principal
-        self.__interest = interest
+        self.principal = principal
+        self.interest = interest
 
     @round_currency
     def get_principal(self):
-        return self.__principal
+        return self.principal
 
     @round_currency
     def get_interest(self):
-        return self.__interest
+        return self.interest
 
     @property
-    @round_currency
-    def value(self):
-        return self.__principal + self.__interest
+    def total(self):
+        return self.principal + self.interest
 
     @round_currency
+    def get_rounded_total(self):
+        return self.total
+
     def __add__(self, other):
-        return self.value + other.value
+        try:
+            return self.total + other.total
+        except AttributeError:
+            return self.total + other
 
     def __radd__(self, other):
         if other == 0:
@@ -35,7 +35,7 @@ class Installment:
             return self.__add__(other)
 
     def __repr__(self):
-        return f'{self.__class__.__name__}({self.__principal}, {self.__interest})'
+        return f'{self.__class__.__name__}({self.principal}, {self.interest})'
 
     def __str__(self):
-        return f'total={self.value:.2f}; principal={self.__principal:.2f}; interest={self.__interest:.2f}'
+        return f'total={self.get_rounded_total():.2f}; principal={self.principal:.2f}; interest={self.interest:.2f}'
