191d8607aa6c746c386d19797ecaee87cd551e7c
jlatala <12474086+jlatala@users.noreply.github.com>
2021-07-07 21:06:19 +0200

    use pandas DataFrame to handle installments

--- credit_calculator/src/credit_calculator.py
+++ credit_calculator/src/credit_calculator.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from .utils import round_currency
 
 
@@ -11,35 +12,38 @@ class CreditCalculator:
     def new_credit(self, credit):
         self.__credit = credit
         self.__installments = []
+        self.credit_df = None
 
-    def add_overpayment(self, overpayment, o_type='decrease_installment'):
+    def add_overpayment(self, overpayment, o_type="decrease_installment"):
         self.__overpayments.update(overpayment)
         self.__overpayment_type = o_type
 
     def calculate(self, init_month=1):
-        for month, installment in enumerate(self.__credit, start=init_month):
-            self.__installments.append(installment)
-            if month in self.__overpayments:
-                amount = self.__overpayments[month]
-                self.__credit = self.__credit.overpay(
-                    amount, self.__overpayment_type)
-                self.calculate(month + 1)
-                break
+        # for month, installment in enumerate(self.__credit, start=init_month):
+        #     self.__installments.append(installment)
+        #     if month in self.__overpayments:
+        #         amount = self.__overpayments[month]
+        #         self.__credit = self.__credit.overpay(
+        #             amount, self.__overpayment_type)
+        #         self.calculate(month + 1)
+        #         break
 
-    def get_installments(self):
-        return self.__installments[:]
+        self.credit_df = pd.DataFrame(self.__credit)
+
+    def get_number_of_installments(self):
+        return len(self.credit_df)
 
     @round_currency
     def get_total(self):
-        return sum(self.__installments) + self.get_total_overpayment()
+        return sum(self.credit_df.sum())
 
     @round_currency
     def get_total_principal(self):
-        return sum([i['principal'] for i in self.__installments])
+        return self.credit_df["principal"].sum()
 
     @round_currency
     def get_total_interest(self):
-        return sum([i['interest'] for i in self.__installments])
+        return self.credit_df["interest"].sum()
 
     def get_total_overpayment(self):
-        return sum(self.__overpayments.values())
+        return self.credit_df["overpayment"].sum()
