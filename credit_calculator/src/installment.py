91f795d27dbfb1548cd644347e703b19bfa49e64
jlatala <12474086+jlatala@users.noreply.github.com>
2021-07-08 16:31:27 +0200

    adding 'overpayment' key to Installment dict

--- credit_calculator/src/installment.py
+++ credit_calculator/src/installment.py
@@ -2,9 +2,10 @@ from .utils import round_currency
 
 
 class Installment(dict):
-    def __init__(self, principal, interest):
+    def __init__(self, principal, interest, overpayment=0.0):
         self["principal"] = principal
         self["interest"] = interest
+        self["overpayment"] = overpayment
 
     @round_currency
     def get_principal(self):
@@ -14,9 +15,13 @@ class Installment(dict):
     def get_interest(self):
         return self["interest"]
 
+    @round_currency
+    def get_overpayment(self):
+        return self["overpayment"]
+
     @property
     def total(self):
-        return self["principal"] + self["interest"]
+        return self["principal"] + self["interest"] + self["overpayment"]
 
     @round_currency
     def get_rounded_total(self):
@@ -35,7 +40,7 @@ class Installment(dict):
             return self.__add__(other)
 
     def __repr__(self):
-        return f"{self.__class__.__name__}({self['principal']}, {self['interest']})"
+        return f"{self.__class__.__name__}({self['principal']}, {self['interest']}, {self['overpayment']})"
 
     def __str__(self):
-        return f"total={self.get_rounded_total():.2f}; principal={self['principal']:.2f}; interest={self['interest']:.2f}"
+        return f"total={self.get_rounded_total():.2f}; principal={self['principal']:.2f}; interest={self['interest']:.2f}; overpayment={self['overpayment']:.2f}"
