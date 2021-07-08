c32b12b681919071b881e7f85b1680d758ef83f9
jlatala <12474086+jlatala@users.noreply.github.com>
2021-07-09 00:24:04 +0200

    rearranging layout - adding sidebar

--- credit_calculator/web/callbacks/callbacks.py
+++ credit_calculator/web/callbacks/callbacks.py
@@ -13,21 +13,45 @@ cc = CreditCalculator()
     Output("pie-chart", "figure"),
     Output("df-table", "data"),
     Input("submit-button", "n_clicks"),
-    State("loan", "value"),
-    State("years", "value"),
+    Input("interest-rate", "n_submit"),
+    State("loan-slider", "value"),
+    State("loan-term-slider", "value"),
     State("interest-rate", "value"),
 )
-def update_figures(n_clicks, loan, years, interest_rate):
-    credit = FixedCredit(loan, years * 12, interest_rate)
+def update_figures(n_clicks, n_submit, loan, years, interest_rate):
+
+    credit = FixedCredit(loan, years * 12, interest_rate / 100)
     cc.new_credit(credit)
-    cc.add_overpayment({36 + i: 1000 for i in range(24)})
-    cc.add_overpayment({100 + i: 1500 for i in range(12)})
+    # cc.add_overpayment({36 + i: 1000 for i in range(24)})
+    # cc.add_overpayment({100 + i: 1500 for i in range(12)})
     cc.calculate()
+
     df = cc.credit_df
+
     bar_fig = px.bar(df, barmode="group")
     bar_fig.update_layout(transition_duration=500)
 
     pie_fig = px.pie(df.sum(), values=0)
     pie_fig.update_layout(transition_duration=1000)
 
+    df["total"] = df.sum(axis=1)
+
     return bar_fig, pie_fig, df.to_dict(orient="records")
+
+
+@app.callback(
+    Output("loan-text", "children"),
+    Input("loan-slider", "value"),
+)
+def update_loan_amount(loan):
+    return f"Loan amount {loan:,}$"
+
+
+@app.callback(
+    Output("loan-term-text", "children"),
+    Input("loan-term-slider", "value"),
+)
+def update_loan_length(years):
+    if years == 1:
+        return f"{years} year"
+    return f"{years} years"
--- credit_calculator/web/layouts/layout.py
+++ credit_calculator/web/layouts/layout.py
@@ -1,32 +1,96 @@
+import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from dash_table import DataTable, FormatTemplate
 
+NAVBAR_STYLE = {"font-size": "40px"}
+
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 70,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+navbar = dbc.NavbarSimple(
+    brand="Mortgage Calculator",
+    brand_href="#",
+    brand_style=NAVBAR_STYLE,
+    color="primary",
+    dark=True,
+    sticky="top",
+)
+
+sidebar = html.Div(
+    [
+        html.P(id="loan-text", className="lead"),
+        dcc.Slider(
+            id="loan-slider",
+            min=50_000,
+            max=1_500_000,
+            step=50_000,
+            value=300_000,
+            marks={50_000: "50k", 500_000: "500k", 1_000_000: "1M", 1_500_000: "1.5M"},
+        ),
+        html.Hr(),
+        html.P(id="loan-term-text", className="lead"),
+        dcc.Slider(
+            id="loan-term-slider",
+            min=1,
+            max=40,
+            step=1,
+            value=20,
+            marks={x: str(x) for x in [1, 10, 20, 30, 40]},
+        ),
+        html.Hr(),
+        html.P(id="interest-rate-text", className="lead"),
+        dcc.Input(
+            id="interest-rate",
+            type="number",
+            min=0,
+            max=100,
+            step=0.01,
+            value=5,
+            placeholder="interest rate",
+        ),
+        html.Hr(),
+        html.Button(id="submit-button", n_clicks=0, children="Submit"),
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+content = html.Div(id="page-content", style=CONTENT_STYLE)
 
 money_format = FormatTemplate.money(2)
+
+content.children = [
+    dcc.Graph(id="bar-chart"),
+    dcc.Graph(id="pie-chart"),
+    DataTable(
+        id="df-table",
+        columns=[
+            {"name": col, "id": col, "type": "numeric", "format": money_format}
+            for col in ["principal", "interest", "overpayment", "total"]
+        ],
+    ),
+]
 
 
 def get_layout():
     layout = html.Div(
         children=[
-            html.H1(children="Mortgage Calculator", style={"textAlign": "center"}),
-            html.Div(
-                children="Web Application - Mortgage Calculator",
-                style={"textAlign": "center"},
-            ),
-            dcc.Input(id="loan", type="number", value=300_000),
-            dcc.Input(id="years", type="number", value=20),
-            dcc.Input(id="interest-rate", type="number", value=0.05),
-            html.Button(id="submit-button", n_clicks=0, children="Submit"),
-            dcc.Graph(id="bar-chart"),
-            dcc.Graph(id="pie-chart"),
-            DataTable(
-                id="df-table",
-                columns=[
-                    {"name": col, "id": col, "type": "numeric", "format": money_format}
-                    for col in ["principal", "interest", "overpayment"]
-                ],
-            ),
+            navbar,
+            sidebar,
+            content,
         ],
     )
 
