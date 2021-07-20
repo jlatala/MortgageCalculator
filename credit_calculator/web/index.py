603624300db0fadcb5387f741606518dec9f9513
jlatala <12474086+jlatala@users.noreply.github.com>
2021-07-20 17:55:55 +0200

    add cmd arguments

--- credit_calculator/web/index.py
+++ credit_calculator/web/index.py
@@ -1,9 +1,13 @@
+import argparse
 from credit_calculator.web.app import app
 from credit_calculator.web.layouts.layout import get_layout
 import credit_calculator.web.callbacks.callbacks
 
 
-app.layout = get_layout()
-
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    parser = argparse.ArgumentParser(description="Run Mortgage Calculator web server")
+    parser.add_argument("--debug", action="store_true", help="Debug mode")
+    args = parser.parse_args()
+
+    app.layout = get_layout()
+    app.run_server(debug=args.debug)
